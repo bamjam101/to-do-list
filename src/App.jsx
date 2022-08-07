7d5fede1a8e1cf20e4150c155e376d366a7745aa
bamjam101 <herejamil123@gmail.com>
2022-08-07 11:16:52 +0530

    Initial commit, setup of reactjs env

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const App = () => {
+  return;
+  <>
+    <div className="container">
+      <div className="header">
+        <h1>The Day Schedule</h1>
+      </div>
+
+      <div className="input">
+        <input type="text" placeholder="Add To List" required />
+        <button>+</button>
+      </div>
+
+      <div className="list">
+        <a>x</a>
+        <ul>
+          <li>Demo</li>
+        </ul>
+      </div>
+    </div>
+  </>;
+};
+
+export default App;
